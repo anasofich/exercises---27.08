d824f9f81335108b069a44a5eced333fee83445a
anasofich <castellanos_chopas@hotmail.com>
2021-08-30 16:22:05 -0500

    Finished capitalization

---
+++ capitalization3.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//First combination
+const myName = "sofia";
+
+const comb1 = myName.substring(0, 2).toLowerCase() + myName.substring(2, 3).toUpperCase() + myName.substring(3, 5).toLowerCase();
+
+console.log(comb1);
+
+//Second combination
+
+const comb2 = myName.substring(0, 1).toUpperCase() + myName.substring(1, 2).toLowerCase() + myName.substring(2, 3).toLocaleUpperCase() + myName.substring(3, 4).toLowerCase() + myName.substring(4, 5).toUpperCase();
+
+console.log(comb2);
--- nameparts.js
+++ nameparts.js
@@ -1,10 +1,10 @@
 "use strict";
+//Trying first name
 const name1 = "Peter Heronimous Lind";
 const firstSpace = name1.indexOf(" ");
 const lastSpace = name1.lastIndexOf(" ");
 
-const firstName1 = name1.substring(0, 5);
-const firstName1opt2 = name1.substring(0, firstSpace);
+const firstName1 = name1.substring(0, firstSpace);
 
 const middleName1 = name1.split(" ")[1];
 const middleName1op2 = name1.substring(firstSpace + 1, lastSpace);
@@ -12,7 +12,23 @@ const middleName1op2 = name1.substring(firstSpace + 1, lastSpace);
 const lastName1 = name1.substring(lastSpace + 1);
 
 console.log(firstName1);
-console.log(firstName1opt2);
 console.log(middleName1);
 console.log(middleName1op2);
 console.log(lastName1);
+
+//Trying second name
+const name2 = "Ana Sofia Castellanos";
+const firstSpace2 = name2.indexOf(" ");
+const lastSpace2 = name2.lastIndexOf(" ");
+
+const firstName2 = name2.substring(0, firstSpace2);
+
+const middleName2 = name2.split(" ")[1];
+const middleName2op2 = name2.substring(firstSpace2 + 1, lastSpace2);
+
+const lastName2 = name2.substring(lastSpace2 + 1);
+
+console.log(firstName2);
+console.log(middleName2);
+console.log(middleName2op2);
+console.log(lastName2);
