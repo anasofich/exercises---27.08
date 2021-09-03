57df26de3d0cc6d49cf44cdd59b10f4ac45a5d09
anasofich <castellanos_chopas@hotmail.com>
2021-09-03 01:20:15 -0500

    added for loops

---
+++ forloops.js
@@ -0,0 +1,52 @@
+"use strict";
+/* for (let counter = 0; counter < 10; counter++) {
+  console.log(counter);
+}
+
+console.log(`after the loop, counter is ${counter}`);
+ */
+
+/* exercises */
+
+/* from 0 to 9 */
+/* for (let counter = 0; counter <= 9; counter++) {
+  console.log(counter);
+} */
+
+/* from 1 to 10 */
+/* for (let counter = 0; counter <= 10; counter++) {
+  console.log(counter);
+}
+ */
+
+/* from 10 to 0 */
+// couldn't make liftoff work
+/* let counter;
+for (let counter = 10; counter >= 0; counter--) {
+  console.log(counter);
+}
+if ((counter = 0)) {
+  console.log("liftoff");
+}
+ */
+
+/* from 1 to 19 - odd numbers */
+/* for (let counter = 1; counter <= 19; counter += 2) {
+  console.log(counter);
+} */
+
+/* from 1 to 16777216 - doubling */
+/* for (let counter = 0; counter <= 167; counter++) {
+  console.log(counter);
+}
+ */
+
+/* from 111 to 138 - adding 3 */
+/* for (let counter = 111; counter <= 138; counter += 3) {
+  console.log(counter);
+} */
+
+/* from 100 to 0 in steps of 10 */
+for (let counter = 100; counter >= 0; counter -= 10) {
+  console.log(counter);
+}
